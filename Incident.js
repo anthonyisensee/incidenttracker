7e19708773165d0de12fdfce44b328b0806d0893
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2024-04-04 17:53:30 -0400

    feat: adds incident storage

--- Incident.js
+++ Incident.js
@@ -2,7 +2,7 @@ export class Incident {
 
     constructor(incidentObject) {
         
-        this.date = incidentObject.date ?? new Date()
+        this.date = incidentObject.date ?? (new Date()).toISOString()
         this.description = incidentObject.description ?? ""
 
     }
--- Main.js
+++ Main.js
@@ -3,12 +3,47 @@ import { Incident } from "./Incident.js"
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let incidents = []
-    
+    // Commonly used variables
+    const storage = new LocalStorageInterface()
     const incidents_container = document.getElementById("incidents_container")
-    
-    // const storage = new LocalStorageInterface()
 
+    // Manage the name of the service for which incidents are being tracked.
+    let service_name = storage.get("service_name")
+
+    if (!service_name) {
+
+        service_name = prompt("Enter the name of the service you wish to track incidents for:")
+
+        storage.set("service_name", service_name)
+
+    }
+
+    document.querySelectorAll('span.service-name').forEach(el => {
+        el.innerHTML = service_name
+    })
+
+    // Load in any previously created incidents
+    let incidents = []
+
+    const stored_incidents = storage.get("incidents")
+
+    if (stored_incidents) {
+
+        incidents_container.innerHTML = ""
+
+        stored_incidents.forEach(incident => {
+
+            const classed_incident = new Incident(incident)
+
+            incidents.push(classed_incident)
+
+            incidents_container.innerHTML = incidentHtml(classed_incident) + incidents_container.innerHTML
+
+        })
+
+    }
+
+    // Add functionality to page buttons
     document.getElementById("create_new_incident").addEventListener("click", () => {
 
         const incident_description_element = document.getElementById("incident_description")
@@ -24,6 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         incidents.push(incident)
+
+        storage.set("incidents", incidents)
 
         incidents_container.innerHTML = incidentHtml(incident) + incidents_container.innerHTML
 
@@ -44,9 +81,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (confirm("Are you sure you want to clear all incidents? This cannot be undone.")) {
 
-            incidents = []
-
             incidents_container.innerHTML = ""
+            incidents = []
+            storage.remove("incidents")
 
         }
 
